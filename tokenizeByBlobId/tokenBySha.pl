dfcc2d47ebdc6a5fc97ee1af8e52f4ae11836d64
D German <dmg@uvic.ca>
2017-04-12 02:10:03 -0700

    Added tokenizer to be used by bfg when processing the repo. it is not generic enough to support C, C++ and Java

---
+++ tokenizeByBlobId/tokenBySha.pl
@@ -0,0 +1,132 @@
+#!/usr/bin/perl
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# this program is run by bfg. It does not take any parameters. Instead, it simply reads
+# its parameters from the environment
+
+# Env. variables
+
+# BFG_BLOB: the id of the blob (a SHA)
+# BFG_FILENAME: basename of the file to process. it has an extension
+# BFG_MEMO_DIR: directory of memoized files
+# BFG_TOKENIZE_CMD: command to tokenize, might include parameters
+
+# this program must not have any parameters
+
+
+# it should  get all its parameters from the environment
+
+ 
+use Digest::SHA qw(sha1_hex);
+use DBI;
+use File::Temp qw/ tempfile tempdir mkstemp/;
+use strict;
+use File::Path qw(make_path);
+use File::Copy;
+
+
+#my $shaDir = '/home/replay/git/token.sha1/';
+#my $shaDir = '/home/replay/linux/token.sha/';
+#my $shaDir = '/tmp/token.sha/';
+
+my %mapLang = (
+               "h" => 'C',
+               "c" => 'C',
+               "hpp" => 'C++',
+               "h++" => 'C++',
+               "java" => 'Java',
+               "cpp" => 'C++',
+               "c++" => 'C++',
+              );
+
+
+if (not defined($ENV{BFG_MEMO_DIR}) ||  $ENV{BFG_MEMO_DIR} eq "") {
+    die "You must define the environment variable BFG_MEMO_DIR equal to the directory where to memoize"
+}
+
+my $shaDir = $ENV{BFG_MEMO_DIR};
+
+if ($shaDir eq "") {
+    die "Directory to use to memoize not set. Use BFG_MEMO_DIR environment variable to set"
+}
+
+my $tokenizeCmd = $ENV{BFG_TOKENIZE_CMD};
+
+if ($tokenizeCmd eq "") {
+    die ("Tokenize command not defined. Use environment variable BFG_TOKENIZE_CMD");
+}
+
+
+my $contents;
+
+die "Sha dir [$shaDir] does not exist" if not -d $shaDir;
+
+while (<>) {
+    $contents .= $_;
+}
+
+my $sha1 = sha1_hex($contents);
+
+#`printf "--------------\n"`;
+#`printf "$sha1\n" >> /speed/tmp/output.txt`;
+
+#`printenv | grep BFG  >> /speed/tmp/output.txt`;
+
+my $dir = $shaDir . '/' . substr($sha1, 0,2) . '/' . substr($sha1, 2,2);
+my $filename = $shaDir . '/' . substr($sha1, 0,2) . '/' . substr($sha1, 2,2) . '/' . $sha1;
+
+my $blob = $ENV{BFG_BLOB};
+my $blobFN = $ENV{BFG_FILENAME};
+
+my $fileExt;
+
+if ($blobFN =~ /\.([^.]+)/) {
+    $fileExt = $1;
+}
+
+if (not defined($mapLang{$fileExt})) {
+    die "unknown file extension [$fileExt]";
+}
+
+if (-f $filename) {
+    print `cat $filename`;
+} else {
+
+  my ($fh, $file) = mkstemp( "tmpfile-in-XXXXX" );
+  my ($fout, $outfile) = mkstemp( "tmpfile-out-XXXXX" );
+
+  print $fh $contents;
+  close $fh;
+
+  my $langOp = "--language=" . $mapLang{$fileExt};
+
+  open(PROC, "$tokenizeCmd $langOp $file |") or die "unable to execute tokenSrcML (verify variable BFG_TOKENIZE_CMD)";
+
+  while (<PROC>) {
+      print $_;
+      print $fout $_;
+  }
+  close PROC;
+  close $fout;
+  if (not -d $dir) {
+      make_path($dir);
+  }
+
+  move( $outfile, $filename) or die "The move operation to memoized directory failed: $!";
+
+  unlink($file)
+
+}

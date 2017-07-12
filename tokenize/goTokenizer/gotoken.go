7ccfb7a09b943cb4fc9f4da325f3932f4dbc603d
D German <dmg@uvic.ca>
2017-07-12 05:14:22 -0700

    added go tokenizer

---
+++ tokenize/goTokenizer/gotoken.go
@@ -0,0 +1,77 @@
+// This program was based on the example that is part of go:
+//
+// go/scanner/example_test.go
+//
+// with  Copyright 2012 The Go Authors and originally licensed
+// as go (BSD style license).
+// 
+// Further modifications by daniel german
+// 
+  
+package main
+  
+import (
+   "fmt"
+   "go/scanner"
+   "go/token"
+   "io/ioutil"
+   "os"
+)
+
+  
+    func Scanner_Scan(src []byte) {
+
+  	// Initialize the scanner.
+  	var s scanner.Scanner
+  	fset := token.NewFileSet()                      // positions are relative to fset
+  	file := fset.AddFile("", fset.Base(), len(src)) // register input "file"
+  	s.Init(file, src, nil /* no error handler */, scanner.ScanComments)
+
+        fmt.Printf("-:-\tbegin_unit\n");
+
+  	// Repeated calls to Scan yield the token sequence found in the input.
+  	for {
+  		pos, tok, lit := s.Scan()
+  		if tok == token.EOF {
+  			break
+  		}
+                if (lit == "") {
+                   fmt.Printf("%s\t%s\n", fset.Position(pos), tok)
+                } else {
+                   fmt.Printf("%s\t%s|%q\n", fset.Position(pos), tok, lit)
+                }
+
+        }
+        
+        fmt.Printf("-:-\tend_unit\n");
+  
+  	// output:
+  	// 1:1	IDENT	"cos"
+  	// 1:4	(	""
+  	// 1:5	IDENT	"x"
+  	// 1:6	)	""
+  	// 1:8	+	""
+  	// 1:10	IMAG	"1i"
+  	// 1:12	*	""
+  	// 1:13	IDENT	"sin"
+  	// 1:16	(	""
+  	// 1:17	IDENT	"x"
+  	// 1:18	)	""
+  	// 1:20	;	"\n"
+  	// 1:20	COMMENT	"// Euler"
+  }
+  
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+
+    dat, err := ioutil.ReadFile(os.Args[1])
+
+    check(err)
+
+    Scanner_Scan(dat)
+}

edd0423e5d7962a38b4c5f26e75e1bb705e2898a
zkchen <wuhankein@gmail.com>
2019-01-15 10:59:16 -0800

    save my own config

--- prettyPrint/build.sh
+++ prettyPrint/build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-LOCAL_PREVIEW=yes
-OUTPUT_DIR="Output"
+LOCAL_PREVIEW=no
+OUTPUT_DIR="/home/zkchen/temp"
+HOST_URL="http://o.cs.uvic.ca:20810/~zkchen"
 GIT_URL="http://github.com/git/git"
 
-ORIGINAL_REPO="../../original.repo/git"
-BLAME_DIRECTORY="../../2.17/blame"
-TOKEN_DIRECTORY="../../token"
-PERSONS_DB="../../2.17/token.db"
-TOKEN_DB="../../2.17/persons.db"
-FLAGS="--filter-lang=c --git-url=${GIT_URL}"
+HOME_REPO="/home/zkchen/cregit-data/git"
+ORIGINAL_REPO="${HOME_REPO}/original.repo-v2.17/git"
+BLAME_DIRECTORY="${HOME_REPO}/v2.17/blame"
+TOKEN_DIRECTORY="${HOME_REPO}/v2.17/token.line"
+PERSONS_DB="${HOME_REPO}/v2.17/persons.db"
+TOKEN_DB="${HOME_REPO}/v2.17/token.db"
+FLAGS="--filter-lang=c --git-url=${GIT_URL} --verbose" # enable debug mode
 
 if [ "$LOCAL_PREVIEW" = "yes" ]; then
 	FLAGS+=" --webroot-relative"
+else
+	FLAGS+=" --webroot=${HOST_URL}"
 fi
 
 set -x
-perl prettyPrint.pl ${FLAGS} "${ORIGINAL_REPO}" "${BLAME_DIRECTORY}" "${TOKEN_DIRECTORY}" "${PERSONS_DB}" "${TOKEN_DB}" "${OUTPUT_DIR}"
-cp -r templates/public/. ${OUTPUT_DIR}/public
+perl prettyPrint.pl ${FLAGS} "${ORIGINAL_REPO}" "${BLAME_DIRECTORY}" "${TOKEN_DIRECTORY}" "${TOKEN_DB}" "${PERSONS_DB}" "${OUTPUT_DIR}"
+#cp -r templates/public/. ${OUTPUT_DIR}/public
 
